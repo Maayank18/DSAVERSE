050137bf4e164b211e39ef81e8916b61796f4592
Maayank18 <maayank51518@gmail.com>
2025-08-30 20:55:01 +0530

    added

--- src/components/core/Course/CourseAccordionBar.jsx
+++ src/components/core/Course/CourseAccordionBar.jsx
@@ -1,7 +1,12 @@
+// import { useDispatch, useSelector } from "react-redux"
+// import { toggleLectureCompletion } from "../../../slices/viewCourseSlice"
 // import CourseSubSectionAccordion from "./CourseSubSectionAccordion"
 // import "./CourseAccordionBar.css"
 
-// export default function CourseAccordionBar({ course, onToggleSubSection }) {
+// export default function CourseAccordionBar({ course }) {
+//   const dispatch = useDispatch()
+//   const { completedLectures } = useSelector((state) => state.viewCourse)
+
 //   return (
 //     <div className="accordion-container">
 //       <div className="accordion-header">
@@ -17,16 +22,11 @@
 //       <div className="accordion-content always-open">
 //         <div className="accordion-subsections">
 //           {course?.subSection?.map((subSec, i) => (
-//             // <CourseSubSectionAccordion
-//             //   subSec={subSec}
-//             //   key={subSec?._id ?? i}
-//             //   isCompleted={subSec?.isCompleted}
-//             //   toggleCompleted={onToggleSubSection}
-//             // />
 //             <CourseSubSectionAccordion
+//               key={subSec?._id ?? i}
 //               courseId={course._id}
 //               subSec={subSec}
-//               isCompleted={completedLectures[course._id]?.includes(subSec._id)}
+//               isCompleted={completedLectures[course._id]?.includes(String(subSec._id))}
 //               toggleCompleted={(courseId, subSecId) =>
 //                 dispatch(toggleLectureCompletion({ courseId, subSecId }))
 //               }
@@ -38,43 +38,64 @@
 //   )
 // }
 
-import { useDispatch, useSelector } from "react-redux"
-import { toggleLectureCompletion } from "../../../slices/viewCourseSlice"
-import CourseSubSectionAccordion from "./CourseSubSectionAccordion"
-import "./CourseAccordionBar.css"
+// components/core/Course/CourseAccordionBar.jsx
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleLectureCompletion } from "../../../slices/viewCourseSlice";
+import CourseSubSectionAccordion from "./CourseSubSectionAccordion";
+import "./CourseAccordionBar.css";
 
 export default function CourseAccordionBar({ course }) {
-  const dispatch = useDispatch()
-  const { completedLectures } = useSelector((state) => state.viewCourse)
+  const dispatch = useDispatch();
+  const { completedLectures } = useSelector((state) => state.viewCourse);
+
+  // defensive defaults
+  const subSections = Array.isArray(course?.subSection) ? course.subSection : [];
+  const lectureCount = subSections.length;
+
+  // completed list for this course (may be undefined if nothing tracked yet)
+  const completedForThisCourse = Array.isArray(completedLectures?.[course?._id])
+    ? completedLectures[course._id]
+    : [];
 
   return (
     <div className="accordion-container">
       <div className="accordion-header">
         <div className="accordion-title">
-          <p>{course?.sectionName}</p>
+          <p>{course?.sectionName ?? "Untitled section"}</p>
         </div>
         <div className="accordion-lectures">
-          <span>{`${course.subSection.length || 0} lecture(s)`}</span>
+          <span>{`${lectureCount} lecture(s)`}</span>
         </div>
       </div>
 
-      {/* 🔥 subsections are always visible, no toggle */}
+      {/* subsections rendered safely */}
       <div className="accordion-content always-open">
         <div className="accordion-subsections">
-          {course?.subSection?.map((subSec, i) => (
-            <CourseSubSectionAccordion
-              key={subSec?._id ?? i}
-              courseId={course._id}
-              subSec={subSec}
-              isCompleted={completedLectures[course._id]?.includes(String(subSec._id))}
-              toggleCompleted={(courseId, subSecId) =>
-                dispatch(toggleLectureCompletion({ courseId, subSecId }))
-              }
-            />
-          ))}
+          {subSections.map((subSec, i) => {
+            // safe ids / fallback to index for key
+            const subId = subSec?._id ?? String(i);
+            const courseIdSafe = course?._id ?? null;
+
+            // completed check must operate on an array
+            const isCompleted = Array.isArray(completedForThisCourse)
+              ? completedForThisCourse.includes(String(subId))
+              : false;
+
+            return (
+              <CourseSubSectionAccordion
+                key={subId}
+                courseId={courseIdSafe}
+                subSec={subSec}
+                isCompleted={isCompleted}
+                toggleCompleted={(courseId, subSecId) =>
+                  dispatch(toggleLectureCompletion({ courseId, subSecId }))
+                }
+              />
+            );
+          })}
         </div>
       </div>
     </div>
-  )
+  );
 }
-
